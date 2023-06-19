6d4741dcb519815d80db83c491eefd2c7ebe829e
wjfu <wjfu@hust.edu.cn>
2023-06-19 21:16:12 +0800

    training a reference model.

--- target_model/training.py
+++ target_model/training.py
@@ -239,7 +239,7 @@ def main(args):
     try:
         if args.dataset == "celeba":
             celeba64_dataset = np.load("./data/celeba64/celeba64.npz")["arr_0"] / 255.0
-            train_data = celeba64_dataset[:10000]
+            train_data = celeba64_dataset[13000:22000]
             eval_data = celeba64_dataset[10000:13000]
         else:
             logger.info(f"\nLoading {args.dataset} data...\n")
