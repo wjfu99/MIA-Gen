337ed110984248702e74f2142a0f5037a1ee6941
wjfu <wjfu@hust.edu.cn>
2023-06-23 22:54:26 +0800

    support prepare data first and then we can directly load data from the files.

--- attack/attack_model.py
+++ attack/attack_model.py
@@ -1,11 +1,14 @@
 import logging
+import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.nn import functional as F
 import sklearn.metrics as metrics
 import numpy as np
+import pickle as pkl
 from tqdm import tqdm
+from attack import utils
 
 logger = logging.getLogger(__name__)
 console = logging.StreamHandler()
@@ -14,6 +17,7 @@ console.setFormatter(formatter)
 logger.addHandler(console)
 logger.setLevel(logging.INFO)
 
+PATH = os.getcwd()
 
 class MLAttckerModel(nn.Module):
     def __init__(self, input_size, hidden_size=128, output_size=3):
@@ -88,7 +92,7 @@ class AttackModel:
         outputs = []
         model.eval()
 
-        for i in tqdm(range(0, num_inputs, batch_size)):
+        for i in range(0, num_inputs, batch_size):
             input_batch = input[i:i + batch_size]
             input_dict = {"data": input_batch}
             output_batch = self.output_reformat(model(input_dict)).loss
@@ -96,7 +100,7 @@ class AttackModel:
         output = np.concatenate(outputs, axis=0)
         return output
 
-    def eval_perturb(self, model, dataset):
+    def eval_perturb(self, model, dataset, per_num=101):
         """
         Evaluate the loss of the perturbed data
 
@@ -108,50 +112,73 @@ class AttackModel:
         var_losses = []
         per_losses = []
         peak_losses = []
-        per_num = 100
         model.eval()
         # revising some original methods of target model.
         self.target_model_revision(model)
         ori_losses = self.generative_model_eval(model, dataset)
-        for data in tqdm(dataset):
-            data = torch.unsqueeze(data, 0)
-            # show_image(data[0])
-            # show_image(eval_loss(data).recon_x.detach()[0])
-            ori_loss = self.generative_model_eval(model, data).loss.item()
-            # masks = mask_tensor(data, prob=0.05, num_masks=per_num)
-            masks = self.gaussian_noise_tensor(data, 0, 0.1, per_num)
-            # masks = add_gaussian_noise(data, noise_scale=0.1, num_noised=per_num)
-            per_loss = []
-            avg_loss = 0
-            for mask in masks:
-                mask = torch.unsqueeze(mask, 0)
-                recon_loss = self.generative_model_eval(model, mask).loss.item()
-                per_loss.append(recon_loss)
-                avg_loss += recon_loss
-            avg_loss = avg_loss / per_num
-            ori_losses.append(ori_loss)
+        for _ in tqdm(range(per_num)):
+            per_dataset = self.gaussian_noise_tensor(dataset, 0, 0.1)
+            per_loss = self.generative_model_eval(model, per_dataset)
             per_losses.append(per_loss)
-            losses.append(avg_loss)
-            var_losses.append(avg_loss - ori_loss)
-            peak_losses.append(np.min(per_loss) - ori_loss)
+        per_losses = np.concatenate(per_losses, axis=1)
+        var_losses = per_losses - ori_losses[:, None]
+        # for data in tqdm(dataset):
+        #     data = torch.unsqueeze(data, 0)
+        #     # show_image(data[0])
+        #     # show_image(eval_loss(data).recon_x.detach()[0])
+        #     ori_loss = self.generative_model_eval(model, data).loss.item()
+        #     # masks = mask_tensor(data, prob=0.05, num_masks=per_num)
+        #     masks = self.gaussian_noise_tensor(data, 0, 0.1, per_num)
+        #     # masks = add_gaussian_noise(data, noise_scale=0.1, num_noised=per_num)
+        #     per_loss = []
+        #     avg_loss = 0
+        #     for mask in masks:
+        #         mask = torch.unsqueeze(mask, 0)
+        #         recon_loss = self.generative_model_eval(model, mask).loss.item()
+        #         per_loss.append(recon_loss)
+        #         avg_loss += recon_loss
+        #     avg_loss = avg_loss / per_num
+        #     ori_losses.append(ori_loss)
+        #     per_losses.append(per_loss)
+        #     losses.append(avg_loss)
+        #     var_losses.append(avg_loss - ori_loss)
+        #     peak_losses.append(np.min(per_loss) - ori_loss)
         output = OutputDict(
-            per_losses = np.array(per_losses),
-            ori_losses = np.array(ori_losses),
-            losses = np.array(losses),
-            var_losses = np.array(var_losses),
-            peak_losses = np.array(peak_losses)
+            per_losses=np.array(per_losses),
+            ori_losses=np.array(ori_losses),
+            var_losses=np.array(var_losses),
+            # losses=np.array(losses),
+            # peak_losses=np.array(peak_losses)
         )
         return output
-
-    def attack_model_training(self, epoch_num=100):
+    def data_prepare(self, path="attack/attack_data"):
+        data_path = os.path.join(PATH, path)
         target_model = self.shadow_model
-
         mem_data = self.datasets['shadow']['mem']
         nonmem_data = self.datasets['shadow']['nonmem']
-        mem_dist = self.eval_perturb(target_model, mem_data).per_losses
-        nonmen_dist = self.eval_perturb(target_model, nonmem_data).per_losses
-        ground_truth = torch.concat([torch.zeros(nonmem_data[0]), torch.ones(mem_data.shape[0])]).cuda()
-        feature_dim = mem_dist.shape[-1]
+        mem_path = os.path.join(data_path, "mem_feat.pkl")
+        nonmem_path = os.path.join(data_path, "nonmen_feat.pkl")
+        if not utils.check_files_exist(mem_path, nonmem_path):
+            mem_feat = self.eval_perturb(target_model, mem_data)
+            nonmen_feat = self.eval_perturb(target_model, nonmem_data)
+            utils.save_dict_to_npz(mem_feat, mem_path)
+            utils.save_dict_to_npz(nonmen_feat, mem_path)
+        else:
+            mem_feat = utils.load_dict_from_npz(mem_path)
+            nonmen_feat = utils.load_dict_from_npz(nonmem_path)
+        return mem_feat, nonmen_feat
+
+    def attack_model_training(self, epoch_num=100):
+        # target_model = self.shadow_model
+        #
+        # mem_data = self.datasets['shadow']['mem']
+        # nonmem_data = self.datasets['shadow']['nonmem']
+        # mem_dist = self.eval_perturb(target_model, mem_data).per_losses
+        # nonmen_dist = self.eval_perturb(target_model, nonmem_data).per_losses
+        mem_feat, nonmen_feat = self.data_prepare()
+        mem_feat, nonmen_feat = mem_feat.var_losses, nonmen_feat.var_losses
+        ground_truth = torch.concat([torch.zeros(mem_feat[0]), torch.ones(nonmen_feat.shape[0])]).cuda()
+        feature_dim = mem_feat.shape[-1]
         attack_model = MLAttckerModel(feature_dim)
         optimizer = optim.Adam(attack_model.parameters(), lr=0.001, weight_decay=0.0005)
         # schedular = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[])
@@ -161,10 +188,10 @@ class AttackModel:
             for phase in ['train', 'val']:
                 if phase == 'train':
                     attack_model.train()
-                    predict = attack_model(mem_dist)
+                    predict = attack_model(mem_feat)
                 else:
                     attack_model.eval()
-                    predict = attack_model(nonmen_dist)
+                    predict = attack_model(nonmen_feat)
                 optimizer.zero_grad()
                 loss = criterion(predict, ground_truth)
                 if phase == 'train':
@@ -181,16 +208,11 @@ class AttackModel:
         attack_model()
 
     @staticmethod
-    def gaussian_noise_tensor(tensor, mean=0.0, std=0.1, num_noise=1):
-        noises = []
-        for i in range(num_noise):
-            # create a tensor of gaussian noise with the same shape as the input tensor
-            noise = torch.randn(tensor.shape) * std + mean
-            # add the noise to the original tensor
-            noisy_tensor = tensor + noise
-            # make sure the pixel values are within [0, 1]
-            noisy_tensor = torch.clamp(noisy_tensor, 0.0, 1.0)
-            noises.append(noisy_tensor)
-        noises = torch.stack(noises, dim=0)
-        noises = torch.squeeze(noises)
-        return noises
+    def gaussian_noise_tensor(tensor, mean=0.0, std=0.1):
+        # create a tensor of gaussian noise with the same shape as the input tensor
+        noise = torch.randn(tensor.shape) * std + mean
+        # add the noise to the original tensor
+        noisy_tensor = tensor + noise
+        # make sure the pixel values are within [0, 1]
+        noisy_tensor = torch.clamp(noisy_tensor, 0.0, 1.0)
+        return noisy_tensor
