4126ac690b9586ea6162da145c31b34c7fc0c34a
wjfu <wjfu@hust.edu.cn>
2023-06-25 09:53:47 +0800

    train a shadow model.

--- target_model/training.py
+++ target_model/training.py
@@ -239,8 +239,8 @@ def main(args):
     try:
         if args.dataset == "celeba":
             celeba64_dataset = np.load("./data/celeba64/celeba64.npz")["arr_0"] / 255.0
-            train_data = celeba64_dataset[0:10000]
-            eval_data = celeba64_dataset[10000:13000]
+            train_data = celeba64_dataset[100000:110000]
+            eval_data = celeba64_dataset[110000:113000]
         else:
             logger.info(f"\nLoading {args.dataset} data...\n")
             train_data = (
