587d9731835861a4f832b21f3910b161e3f105f8
wjfu <wjfu@hust.edu.cn>
2023-06-27 21:20:06 +0800

    load text data and models for attacking.

--- attacker_text.py
+++ attacker_text.py
@@ -14,6 +14,10 @@ import seaborn as sns
 from tqdm import tqdm
 from torchvision.transforms import ToPILImage
 from attack.attack_model import AttackModel
+from SentenceVAE.model import SentenceVAE
+from SentenceVAE.ptb import PTB
+from collections import OrderedDict
+import json
 
 logger = logging.getLogger(__name__)
 console = logging.StreamHandler()
@@ -30,52 +34,50 @@ os.environ["CUDA_VISIBLE_DEVICES"] = str(np.argmax(memory_available))
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-dataset = "ptb"
-target_path = sorted(os.listdir(PATH + '/target_model/target_models_on_' + dataset))[-1]
-target_model = AutoModel.load_from_folder(os.path.join(PATH + '/target_model/target_models_on_'+dataset, target_path, 'final_model'))
-target_model = target_model.to(device)
+## Load text generation model.
+with open("/mnt/data0/fuwenjie/MIA/MIA-Gen/SentenceVAE/data" + '/ptb.vocab.json', 'r') as file:
+    vocab = json.load(file)
+w2i, i2w = vocab['w2i'], vocab['i2w']
 
-reference_path = sorted(os.listdir(PATH + '/target_model/reference_models_on_'+dataset))[-1]
-reference_model = AutoModel.load_from_folder(os.path.join(PATH + '/target_model/reference_models_on_'+dataset, reference_path, 'final_model'))
-reference_model = reference_model.to(device)
+target_path = os.path.join(PATH, 'SentenceVAE/bin/target')
+target_path = os.path.join(target_path, sorted(os.listdir(target_path))[-1])
+with open(target_path + "/model_params.json", 'r') as file:
+    params = json.load(file)
+target_model = SentenceVAE(**params).to("cuda")
+target_model.load_state_dict(torch.load(target_path+"/E9.pytorch"))
 
-shadow_path = sorted(os.listdir(PATH + '/target_model/shadow_models_on_'+dataset))[-1]
-shadow_model = AutoModel.load_from_folder(os.path.join(PATH + '/target_model/shadow_models_on_'+dataset, shadow_path, 'final_model'))
-shadow_model = shadow_model.to(device)
+shadow_path = os.path.join(PATH, 'SentenceVAE/bin/shadow')
+shadow_path = os.path.join(shadow_path, sorted(os.listdir(shadow_path))[-1])
+with open(shadow_path + "/model_params.json", 'r') as file:
+    params = json.load(file)
+shadow_model = SentenceVAE(**params).to("cuda")
+shadow_model.load_state_dict(torch.load(shadow_path+"/E9.pytorch"))
+
+reference_path = os.path.join(PATH, 'SentenceVAE/bin/reference')
+reference_path = os.path.join(reference_path, sorted(os.listdir(reference_path))[-1])
+with open(reference_path + "/model_params.json", 'r') as file:
+    params = json.load(file)
+reference_model = SentenceVAE(**params).to("cuda")
+reference_model.load_state_dict(torch.load(reference_path+"/E9.pytorch"))
+
 logger.info("Successfully loaded models!")
-# last_training = sorted(os.listdir('target_model/my_model'))[-1]
-# trained_model = AutoModel.load_from_folder(os.path.join('target_model/my_model', last_training, 'final_model'))
-# trained_model = trained_model.to(device)
-if dataset == "celeba":
-    celeba64_dataset = np.load("./target_model/data/celeba64/celeba64.npz")["arr_0"] / 255.0
-    datasets = dict(
-        target=dict(
-            mem=torch.Tensor(celeba64_dataset[0:10000]),
-            nonmem=torch.Tensor(celeba64_dataset[10000:13000])
-        ),
-        shadow=dict(
-            mem=torch.Tensor(celeba64_dataset[100000:110000]),
-            nonmem=torch.Tensor(celeba64_dataset[110000:113000])
-        ),
-        reference=dict(
-            mem=torch.Tensor(celeba64_dataset[150000:160000]),
-            nonmem=torch.Tensor(celeba64_dataset[160000:163000])
-        )
-    )
-    logger.info("Successfully loaded datasets!")
-else:
-    logger.info(f"\nLoading {dataset} data...\n")
-    train_data = torch.Tensor(
-            np.load(os.path.join(PATH, f"target_model/data/{dataset}", "train_data.npz"))[
-                "data"
-            ]
-            / 255.0
-    )
-    if dataset == "mnist":
-        train_data = train_data[:-10000]
-    eval_data = torch.Tensor(
-            np.load(os.path.join(PATH, f"target_model/data/{dataset}", "eval_data.npz"))["data"]
-            / 255.0
+
+# Load datasets
+
+model_type = "reference"
+splits = []
+for model_type in ["target", "shadow", "reference"]:
+    for data_type in ["train", "valid"]:
+        splits.append(model_type+"_"+data_type)
+
+datasets = OrderedDict()
+for split in splits:
+    datasets[split] = PTB(
+        data_dir="SentenceVAE/data",
+        split=split,
+        create_data=False,
+        max_sequence_length=60,
+        min_occ=1
     )
 
 attack_model = AttackModel(target_model, datasets, reference_model, shadow_model)
