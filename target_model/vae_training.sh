1498b398f316893f3bffe9ba92cf248c7a8f7d6f
wjfu <wjfu@hust.edu.cn>
2023-07-15 21:57:33 +0800

    training target vae model on the celeba 50k.

---
+++ target_model/vae_training.sh
@@ -0,0 +1,9 @@
+ # target model
+ python training.py \
+  --dataset celeba  \
+  --model_name vae \
+  --model_config './configs/celeba/vae_config.json' \
+  --training_config './configs/celeba/base_training_config.json' \
+
+
+ # target model
