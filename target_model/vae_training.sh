0eaf31535a6c5f2ff90660bcea27c9c84b205e43
wjfu <wjfu@hust.edu.cn>
2023-07-16 21:22:33 +0800

    train shadow model for vae model on 50k dataset.

--- target_model/vae_training.sh
+++ target_model/vae_training.sh
@@ -9,4 +9,24 @@
   --eval_sta_idx=50000 \
   --eval_end_idx=60000
 
- # target model
+ # shadow model
+  python training.py \
+  --dataset celeba  \
+  --model_name vae \
+  --model_config './configs/celeba/vae_config.json' \
+  --training_config './configs/celeba/base_training_config.json' \
+  --train_sta_idx=60000 \
+  --train_end_idx=110000 \
+  --eval_sta_idx=110000 \
+  --eval_end_idx=120000
+
+   # reference model
+  python training.py \
+  --dataset celeba  \
+  --model_name vae \
+  --model_config './configs/celeba/vae_config.json' \
+  --training_config './configs/celeba/base_training_config.json' \
+  --train_sta_idx=120000 \
+  --train_end_idx=170000 \
+  --eval_sta_idx=170000 \
+  --eval_end_idx=180000
