02c27092d95416c990a309cc45cb2ce7f80186cf
wjfu <wjfu@hust.edu.cn>
2023-08-11 20:46:55 +0800

    study on perturbation mechanism: brightness(VAE).

--- attack/attack_model_diffusion.py
+++ attack/attack_model_diffusion.py
@@ -355,8 +355,8 @@ class AttackModel:
             # gen_feat = gen_feat[:, 2, :]
 
         if cfg["attack_kind"] == "stat":
-            mem_feat = mem_feat[:, :, 9]
-            nonmem_feat = nonmem_feat[:, :, 9]
+            mem_feat = mem_feat[:, :, 8]
+            nonmem_feat = nonmem_feat[:, :, 8]
             mem_feat[np.isnan(mem_feat)] = 0
             nonmem_feat[np.isnan(nonmem_feat)] = 0
             feat = np.concatenate([mem_feat.mean(axis=(-1)), nonmem_feat.mean(axis=(-1))])
@@ -706,8 +706,9 @@ class AttackModel:
     def image_dataset_perturbation(dataset, strength):
         perturbation = transforms.Compose([
             transforms.ToTensor(),
-            transforms.CenterCrop(size=int(64 * strength)),
-            transforms.Resize(size=64),
+            # transforms.CenterCrop(size=int(64 * strength)),
+            # transforms.Resize(size=64),
+            transforms.ColorJitter(brightness=(strength, strength)),
         ])
         def transform_images(examples):
             images = [perturbation(image.convert("RGB")) for image in examples["image"]]
