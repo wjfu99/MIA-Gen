78ade4fea2fbc131c3f29fe57495f2e01965bf10
wjfu <wjfu@hust.edu.cn>
2023-08-11 15:02:34 +0800

    study on perturbation mechanism: contrast.

--- attack/attack_model_diffusion.py
+++ attack/attack_model_diffusion.py
@@ -688,7 +688,7 @@ class AttackModel:
             # transforms.RandomResizedCrop(size=(64, 64), scale=(0.8, 0.8)),
             # transforms.CenterCrop(size=int(64 * strength)),
             # transforms.Resize(size=64),
-            transforms.ColorJitter(brightness=(strength, strength)),
+            transforms.ColorJitter(contrast=(strength, strength)),
             transforms.Normalize([0.5], [0.5]),
         ])
         def transform_images(examples):
