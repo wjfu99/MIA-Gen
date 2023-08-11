cdb43cfb66124b18200e4484feb5731a1397037d
wjfu <wjfu@hust.edu.cn>
2023-08-11 22:36:37 +0800

    study on perturbation mechanism: crop(VAE).

--- attack/attack_model_diffusion.py
+++ attack/attack_model_diffusion.py
@@ -355,8 +355,8 @@ class AttackModel:
             # gen_feat = gen_feat[:, 2, :]
 
         if cfg["attack_kind"] == "stat":
-            mem_feat = mem_feat[:, :, 7]
-            nonmem_feat = nonmem_feat[:, :, 7]
+            mem_feat = mem_feat[:, :, 5]
+            nonmem_feat = nonmem_feat[:, :, 5]
             mem_feat[np.isnan(mem_feat)] = 0
             nonmem_feat[np.isnan(nonmem_feat)] = 0
             feat = np.concatenate([mem_feat.mean(axis=(-1)), nonmem_feat.mean(axis=(-1))])
@@ -710,7 +710,7 @@ class AttackModel:
             # transforms.Resize(size=64),
             # transforms.ColorJitter(brightness=(strength, strength)),
             # transforms.RandomPerspective(distortion_scale=strength, p=1),
-            transforms.Resize(size=int(strength)),
+            transforms.CenterCrop(size=int(64 * strength)),
             transforms.Resize(size=64),
         ])
         def transform_images(examples):
